671bd7ff89bc76cc840b415bbd212e720efc5ee2
lee-simon625 <simbad625@gmail.com>
2021-04-27 21:08:03 +0100

    commiting

--- src/Main.java
+++ src/Main.java
@@ -28,87 +28,82 @@ public class Main {
 
             System.out.print("\nWhich option:   ");
 
-            int option = keyboard.nextInt();
+            int option = Integer.parseInt(keyboard.nextLine());
             System.out.println();
+            System.out.println("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+            switch (option){
 
-            if (option == 1) {
+                case 1:
+                    campaign.campaignInfo();
+                case 2:
+                    campaign.territoryInfo();
+                case 3:
+                    createTerritory(keyboard, campaign);
+                case 4:
+                    removeTerritory(keyboard, campaign);
+                case 5:
+                    campaign.gangInfo();
+                case 6:
+                    createGang(keyboard, campaign);
+                case 7:
+                    removeGang(keyboard, campaign);
+                case 8:
+                    break;
 
-                System.out.println("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
-                campaign.campaignInfo();
-
-            } else if (option == 2) {
-
-                System.out.println("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
-                campaign.territoryInfo();
-
-            } else if (option == 3) {
-
-                System.out.println("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
-
-                System.out.println("Enter territories name:    ");
-                String usless = keyboard.nextLine();
-                String name = keyboard.nextLine();
-                System.out.println("Enter the income: ");
-                String income = keyboard.nextLine();
-                System.out.println("Enter what it can recruit:  ");
-                String recruit = keyboard.nextLine();
-                System.out.println("Enter any equipment it gives: ");
-                String equipment = keyboard.nextLine();
-                System.out.println("Enter any special bonuses it provides: ");
-                String special = keyboard.nextLine();
-                System.out.println("Enter any reputation it can give: ");
-                int reputation = keyboard.nextInt();
-
-                campaign.createTerritory(name, income, recruit, equipment, special, reputation);
-
-            } else if (option == 4) {
-
-                System.out.println("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
-                System.out.print(("Enter the ID of the territory to delete:  "));
-                int id = keyboard.nextInt();
-                campaign.removeTerritory(id);
-
-            } else if (option == 5) {
-
-                System.out.println("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
-                campaign.gangInfo();
-
-            } else if (option == 6) {
-
-                System.out.println("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
-
-                System.out.println("Enter player's name:    ");
-                String useless = keyboard.nextLine();
-                String player = keyboard.nextLine();
-                System.out.println("Enter the gang's name: ");
-                String name = keyboard.nextLine();
-                System.out.println("Enter what type of gang it is:  ");
-                String gangType = keyboard.nextLine();
-                System.out.println("Enter the gangs total vale: ");
-                int totalValue = keyboard.nextInt();
-
-                campaign.createGang(player, name, gangType, totalValue);
-
-            } else if (option == 7) {
-
-                System.out.println("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
-                System.out.print(("Enter the ID of the gang to delete:  "));
-                int id = keyboard.nextInt();
-                campaign.removeGang(id);
-
-            } else if (option == 8) {
-
-                break;
 
             }
+
+
 
 
         }
 
 
+
     }
 
+    private static void createTerritory(Scanner keyboard, Campaign campaign){
 
+        System.out.println("Enter territories name:    ");
+        String name = keyboard.nextLine();
+        System.out.println("Enter the income: ");
+        String income = keyboard.nextLine();
+        System.out.println("Enter what it can recruit:  ");
+        String recruit = keyboard.nextLine();
+        System.out.println("Enter any equipment it gives: ");
+        String equipment = keyboard.nextLine();
+        System.out.println("Enter any special bonuses it provides: ");
+        String special = keyboard.nextLine();
+        System.out.println("Enter any reputation it can give: ");
+        int reputation = keyboard.nextInt();
+
+        campaign.createTerritory(name, income, recruit, equipment, special, reputation);
+    }
+
+    private static void removeTerritory(Scanner keyboard, Campaign campaign){
+        System.out.print(("Enter the ID of the territory to delete:  "));
+        int id = keyboard.nextInt();
+        campaign.removeTerritory(id);
+    }
+
+    private static void createGang(Scanner keyboard, Campaign campaign){
+        System.out.println("Enter player's name:    ");
+        String player = keyboard.nextLine();
+        System.out.println("Enter the gang's name: ");
+        String name = keyboard.nextLine();
+        System.out.println("Enter what type of gang it is:  ");
+        String gangType = keyboard.nextLine();
+        System.out.println("Enter the gangs total vale: ");
+        int totalValue = keyboard.nextInt();
+
+        campaign.createGang(player, name, gangType, totalValue);
+    }
+
+    private static void removeGang(Scanner keyboard, Campaign campaign){
+        System.out.print(("Enter the ID of the gang to delete:  "));
+        int id = keyboard.nextInt();
+        campaign.removeGang(id);
+    }
 }
 
 
